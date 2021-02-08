31482aa1ad2fa78e9bc4601a929617c03c012097
Eren Inanci <erencan_inanci@hotmail.com>
2021-02-08 04:38:16 +0300

    Create SaveLoad.cs

---
+++ Scripts/SaveLoad.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class SaveLoad : MonoBehaviour
+{
+    public TextMeshProUGUI textVal;
+    int val = 0;
+    public void Save()
+    {
+        PlayerPrefs.SetInt("Value", val);
+    }
+
+    public void Load()
+    {
+        val = PlayerPrefs.GetInt("Value", 0);
+        if(PlayerPrefs.HasKey("Value"))
+        {
+            int currentIndex = SceneManager.GetActiveScene().buildIndex;
+            if(currentIndex == val)
+            {
+
+            }
+            else
+            {
+                SceneManager.LoadScene(val);
+            }
+        }
+        textVal.text = val.ToString();
+    }
+
+    public void Delete()
+    {
+        val = 0;
+    }
+
+    public void IncreaseValue()
+    {
+        val++;
+        textVal.text = val.ToString();
+    }
+
+    public void DecreaseValue()
+    {
+        if(val <= 0)
+        {
+            val = 0;
+            textVal.text = val.ToString();
+        }
+        else 
+        {
+            val--;
+            textVal.text = val.ToString();
+        }
+    }
+
+    public void OnAwake()
+    {
+        Delete();
+        Load();
+    }
+
+    public void Start()
+    {
+        Delete();
+        Load();
+    }
+
+    public void Update()
+    {
+        Save();
+    }
+}
